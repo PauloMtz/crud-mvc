e24b53c1fbf0a8f83b4d467a7424ca3f164e4918
PauloMtz <pramosmtz@gmail.com>
2018-09-18 18:10:01 -0300

    Iniciando projeto crud-mvc - estrutura básica MVC

---
+++ config.php
@@ -0,0 +1,29 @@
+<?php
+require 'env.php';
+
+$config = array();
+
+// se mudar para um servidor externo, alterar arquivo env.php
+if (ENVIROMENT == 'development') {
+	define("BASE_URL", "http://127.0.0.1/crud-mvc/");
+	$config['dbname'] = 'cadastro_crud';
+	$config['host'] = '127.0.0.1';
+	$config['user'] = 'root';
+	$config['pass'] = 'toor.971';
+} else {
+	define("BASE_URL", "http://www.site.com.br/");
+	$config['dbname'] = 'dbname';
+	$config['host'] = '255.255.255.0';
+	$config['user'] = 'root';
+	$config['pass'] = 'toor';
+}
+
+global $db_connect;
+
+try {
+	$db_connect = new PDO("mysql:dbname=".$config['dbname'].";host=".$config['host'], $config['user'], $config['pass']);
+} catch(PDOException $e) {
+	echo "Erro ao conectar.<br>" . $e->getMessage();
+	exit;
+}
+?>
---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+session_start();
+require 'config.php';
+
+/*
+* para usar o modo de urls amigáveis
+* criar o arquivo .htaccess
+* e descomentar a linha abaixo no arquivo httpd.conf
+* C:/Apache24/conf
+* #LoadModule rewrite_module modules/mod_rewrite.so
+*/
+
+
+?>
